e1056cc4829f05ee50f2ad3722afcc8d912a7f8e
ibzocraft <113947503+ibzocraft@users.noreply.github.com>
2025-06-22 22:57:14 +0000

    Remove hardcoded database connection settings from config.php to enhance security and allow for environment-specific configurations.

--- includes/config/config.php
+++ includes/config/config.php
@@ -4,10 +4,3 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // Chargement des variables d'environnement depuis .env vers $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
-
-$_ENV['DB_HOST'] = 'localhost';
-$_ENV['DB_NAME'] = 'fila9119_questions ';
-$_ENV['DB_USER'] = 'fila9119_ibzocraft';
-$_ENV['DB_PASSWORD'] = 'eFU(uZ]G2S3l';
-
-$_ENV['BASE_URL'] = 'https://questions.arsonry.com/';
